c62856311ce65e681cbba03789cc3d368460d1da
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-03-16 13:10:59 +0100

    Debug jacoco analysis

--- travis.sh
+++ travis.sh
@@ -9,4 +9,29 @@ function configureTravis {
 }
 configureTravis
 
-regular_mvn_build_deploy_analyze
+set -euo pipefail
+
+echo '======= Build, deploy and analyze master'
+
+  # Fetch all commit history so that SonarQube has exact blame information
+  # for issue auto-assignment
+  # This command can fail with "fatal: --unshallow on a complete repository does not make sense"
+  # if there are not enough commits in the Git repository (even if Travis executed git clone --depth 50).
+  # For this reason errors are ignored with "|| true"
+  #git fetch --unshallow || true
+
+  # Analyze with SNAPSHOT version as long as SQ does not correctly handle
+  # purge of release data
+  SONAR_PROJECT_VERSION=`maven_expression "project.version"`
+
+  # Do not deploy a SNAPSHOT version but the release version related to this build
+  set_maven_build_version $TRAVIS_BUILD_NUMBER
+
+  export MAVEN_OPTS="-Xmx1536m -Xms128m"
+  mvn org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar \
+      -Pcoverage-per-test \
+      -Dmaven.test.redirectTestOutputToFile=false \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_TOKEN \
+      -Dsonar.projectVersion=$SONAR_PROJECT_VERSION \
+      -B -e -V -X
