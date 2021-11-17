2e50e82629568eae38df3fd7decde5236bd41a0d
Julien HENRY <julien.henry@sonarsource.com>
2021-11-17 11:26:13 +0100

    Enable signing

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v58 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v61 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
   source ~/.local/bin/setup_promote_environment
 }
@@ -12,5 +12,5 @@ configureTravis
 
 export DEPLOY_PULL_REQUEST=true
 
-. regular_mvn_build_deploy_analyze
+. regular_mvn_build_deploy_analyze -Psign
 promote
