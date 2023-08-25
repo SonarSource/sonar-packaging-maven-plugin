73dbaff8ebc826ee4050f71ce5808fbd640d9fce
cody-simms-sonarsource <141657208+cody-simms-sonarsource@users.noreply.github.com>
2023-08-25 09:16:55 -0500

    PACKMP-18 Add Scanner-Languages property to manifest (#41)

---
+++ src/it/addRequiredForLanguages/src/main/java/org/sonar/plugins/sample/SamplePlugin.java
@@ -0,0 +1,14 @@
+package org.sonar.plugins.sample;
+
+import org.sonar.api.Plugin;
+
+/**
+ * This class is the entry point for all extensions. It is referenced in pom.xml.
+ */
+public class SamplePlugin implements Plugin {
+
+  @Override
+  public void define(Context context) {
+
+  }
+}
