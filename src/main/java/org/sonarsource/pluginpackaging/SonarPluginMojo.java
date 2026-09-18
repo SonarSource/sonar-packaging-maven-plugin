250f9cf043d675a0d708214a21083493786712fe
Pavel Mikula <pavel.mikula@sonarsource.com>
2026-09-18 14:33:52 +0200

    Remove pluginApiMinVersion implicit fallback

--- src/main/java/org/sonarsource/pluginpackaging/SonarPluginMojo.java
+++ src/main/java/org/sonarsource/pluginpackaging/SonarPluginMojo.java
@@ -157,7 +157,7 @@ public class SonarPluginMojo extends AbstractSonarMojo {
       addManifestProperty(PluginManifestProperty.USE_CHILD_FIRST_CLASSLOADER, String.valueOf(isUseChildFirstClassLoader() != null && isUseChildFirstClassLoader()));
       addManifestProperty(PluginManifestProperty.BASE_PLUGIN, getBasePlugin());
       addManifestProperty(PluginManifestProperty.HOMEPAGE, getPluginUrl());
-      addManifestProperty(PluginManifestProperty.SONAR_VERSION, firstNonNull(getPluginApiMinVersion(), getPluginApiArtifact().getVersion()));
+      addManifestProperty(PluginManifestProperty.SONAR_VERSION, getPluginApiMinVersion());
       addManifestProperty(PluginManifestProperty.LICENSE, getLicensing());
       addManifestProperty(PluginManifestProperty.ORGANIZATION, getPluginOrganizationName());
       addManifestProperty(PluginManifestProperty.ORGANIZATION_URL, getPluginOrganizationUrl());
